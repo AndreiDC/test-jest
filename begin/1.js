0ad8b29c73c51b0108802b542b7439db2279d13f
Andrei <andrei.zelenschi@deepcode.xyz>
2019-03-02 15:10:10 +0200

    begin 1

---
+++ begin/1.js
@@ -0,0 +1,7 @@
+/*
+Given the side a of a square, find the perimeter P of the square: P = 4·a.
+*/
+function perimeter(a) {
+    return a * 4;
+  }
+  module.exports = perimeter;
---
+++ begin/1.test.js
@@ -0,0 +1,5 @@
+const perimeter = require('./1');
+
+test('returns correct result', () => {
+  expect(perimeter(2)).toBe(8);
+});
