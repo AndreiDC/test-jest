aed5f0cce6f9e81f466d194bbf4b33e776efe08d
Andrei <andrei.zelenschi@deepcode.xyz>
2019-03-15 17:08:35 +0200

    begin 25 some modifications

--- begin/25.js
+++ begin/25.js
@@ -3,6 +3,6 @@ const { findY } = require("./25.js");
 test("Given an independent variable x, find the value y", () => {
   expect(findY(2.15)).toBe(261.58);
 });
-test("Given an independent variable x, ", () => {
+test("Given an independent variable x ", () => {
   expect(findY(2.52)).toBe(723.19);
 });
