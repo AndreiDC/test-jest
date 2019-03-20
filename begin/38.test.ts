ab90aad2bfa359642304b890f918b7d091603a31
Andrei <andrei.zelenschi@deepcode.xyz>
2019-03-20 17:09:00 +0200

    begin 38

---
+++ begin/38.test.ts
@@ -0,0 +1,5 @@
+import { soleLinEqu } from "./38";
+
+test(" solve liniar equation ", () => {
+  expect(soleLinEqu(2, -18)).toEqual(9);
+});
---
+++ begin/38.ts
@@ -0,0 +1,7 @@
+/* Solve a linear equation A·x + B = 0 with given coefficients A and B (A is not equal to 0).*/
+
+export function soleLinEqu(a, b: number) {
+  let s: number;
+  s = b / -a;
+  return s;
+}
