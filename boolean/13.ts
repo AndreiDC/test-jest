0780d2ea6f86a9b4839f22df4135a609221e5265
Andrei <andrei.zelenschi@deepcode.xyz>
2019-04-03 21:51:24 +0300

    boolean 13

--- boolean/13.ts
+++ boolean/13.ts
@@ -1,4 +1,5 @@
 /*Given three integers A, B, C, verify the following proposition: "At least one of the numbers A, B, C is positive".*/
+
 export function oneNumber(a, b, c: number) {
   let oN: boolean;
   oN = a > 0 || b > 0 || c > 0;
