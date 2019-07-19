c0aef6497581ba49333be3621e2e0027f749ff53
Andrei <andrei.zelenschi@deepcode.xyz>
2019-07-19 18:17:49 +0300

    while 17

--- while/17.test.ts
+++ while/17.test.ts
@@ -4,5 +4,5 @@ test("while 17 first test", () => {
   expect(loop(1234)).toEqual([4, 3, 2, 1]);
 });
 test("while 17 second test", () => {
-  expect(loop(80)).toEqual([08]);
+  expect(loop(80)).toEqual([0, 8]);
 });
